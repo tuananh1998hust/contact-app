2b4dbd9a4cd7d8c7deac625e9d7b73d83f490105
tuananh1998hust <tuananh1998.hust@gmail.com>
2018-12-27 22:45:07 +0700

    add get create and views create

--- controller/controller.users.js
+++ controller/controller.users.js
@@ -2,5 +2,21 @@ var db = require('../db');
 var shortid = require('shortid');
 
 module.exports.index = (req, res) => {
-	res.render('users/index');
+	res.render('users/index', {
+		users: db.get('users').value()
+	});
+}
+
+module.exports.getCreate = (req, res) => {
+	res.render('users/create');
+}
+
+module.exports.postCreate = (req, res, next) => {
+	req.body.id = shortid.generate();
+
+	db.get('users')
+		.push(req.body)
+		.write();
+
+	res.redirect('/users');
 }
--- routes/routes.users.js
+++ routes/routes.users.js
@@ -2,7 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 const controller = require('../controller/controller.users.js');
+const middleware = require('../middleware/middleware.users.js');
 
 router.get('/', controller.index);
+
+router.get('/create', controller.getCreate);
+
+router.post('/create', middleware.postCreate ,controller.postCreate);
 
 module.exports = router;
