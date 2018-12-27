1c07a7c7cddf2a11f72b049aa458584fde7892e7
tuananh1998hust <tuananh1998.hust@gmail.com>
2018-12-27 16:55:57 +0700

    add file public

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+const routesUsers = require('./routes/routes.users.js');
+
 const port = 3000;
 
 app.set('view engine', 'pug');
@@ -11,6 +13,8 @@ app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.get('/', (req, res) => res.render('index'));
+
+app.use('/users', routesUsers);
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
