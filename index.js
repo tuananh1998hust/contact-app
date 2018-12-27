8fc7ae26523a4fa8faff82b2d940ab8663ca638d
tuananh1998hust <tuananh1998.hust@gmail.com>
2018-12-27 17:07:47 +0700

    Revert "add file layout common.pug"
    This reverts commit a2e92f2bb2dee17fe883448081f7c7d2592944bc.

--- index.js
+++ index.js
@@ -2,20 +2,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-const usersRoutes = require('./routes/routes.users');
+const routesUsers = require('./routes/routes.users.js');
 
 const port = 3000;
 
 app.set('view engine', 'pug');
 app.set('views', './views');
 
-app.use(express.static('public'));
-
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.get('/', (req, res) => res.render('index'));
 
-app.use('/users', usersRoutes);
+app.use('/users', routesUsers);
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- routes/routes.users.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var controller = require('../controller/controller.users');
-
-var router = express.Router();
-
-router.get('/', controller.index);
-
-module.exports = router;
